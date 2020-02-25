27ca6ab371c5da8a942bde520f1bc19ec16ba693
todzhang's cloudsdocker <phray.zhang@gmail.com>
2020-02-25 17:06:41 +1100

    Update TreeInvertBST.java

--- src/main/java/TreeInvertBST.java
+++ src/main/java/TreeInvertBST.java
@@ -3,20 +3,15 @@ package algo;
 public class TreeInvertBST {
     public static void main(String[] args) {
         System.out.printf("===start===");
-
-        System.out.printf("invert tree: "+invertTree(TreeNode.buildBSTTree()));
+        TreeNode root = invertTree(TreeNode.buildBSTTree());
+        System.out.printf("invert tree: "+ root);
     }
 
     static TreeNode invertTree(TreeNode root){
-        if(root==null) {
-            return root;
-        }
-
-        TreeNode tmpLeft = invertTree(root.left);
-        TreeNode tmpRight = invertTree(root.right);
-
-        root.left=tmpRight;
-        root.right=tmpLeft;
+        if(root==null)  return null;
+        TreeNode tmpLeft = root.left;
+        root.left=invertTree(root.right);
+        root.right=invertTree(tmpLeft);
         return root;
     }
 }
