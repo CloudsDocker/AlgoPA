d5fe0668f2500ecf5db2a1999b72aded9b4c33c2
todzhang's cloudsdocker <phray.zhang@gmail.com>
2020-02-26 12:48:11 +1100

    Update LongestCommonPrefix.java

--- src/main/java/LongestCommonPrefix.java
+++ src/main/java/LongestCommonPrefix.java
@@ -10,8 +10,38 @@ public class LongestCommonPrefix {
     }
 
     /*
+    To improve perforamance, better to check if it can sort array first, which will make it more faster
+    The magical point of this solution is:
+    1. To Array sort first, which is log(n)
+    2. Then compare only the 1st and last two string, they should be MOST distinct ones, so only compare those two
+     */
+    public String longestCommonPrefix_faster(String[] strs) {
+        StringBuilder result = new StringBuilder();
+
+        if (strs!= null && strs.length > 0){
+
+            Arrays.sort(strs);
+
+            char [] a = strs[0].toCharArray();
+            char [] b = strs[strs.length-1].toCharArray();
+
+            for (int i = 0; i < a.length; i ++){
+                if (b.length > i && b[i] == a[i]){
+                    result.append(b[i]);
+                }
+                else {
+                    return result.toString();
+                }
+            }
+
+        }
+        return result.toString();
+    }
+
+    /*
     logic is pick one string to for loop, increase return string if character is same for all sub strings, 
     otherwise return saved loggest sub string
+    Runtime: 10 ms, faster than 6.58% of Java online submissions for Longest Common Prefix.
      */
     static String getLonggestCommonPrefix(String[] listWords){
         String strCommonPrefix = "";
@@ -28,4 +58,7 @@ public class LongestCommonPrefix {
         }
         return strCommonPrefix;
     }
+
+    
+    
 }
