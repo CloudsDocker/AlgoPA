954fa86bdccb4f524ef40c6bf4317a662dd10219
todzhang's cloudsdocker <phray.zhang@gmail.com>
2020-02-28 09:03:14 +1100

    Update longestCommonSubsequence.java

--- src/main/java/longestCommonSubsequence.java
+++ src/main/java/longestCommonSubsequence.java
@@ -34,4 +34,22 @@ class Solution {
         return memo[m][n];
         
     }
+    /*
+    Space Optimization
+
+Obviously, the code in method 1 only needs information of previous row to update current row. So we just use a two-row 2D array to save and update the matching results for chars in s1 and s2.
+
+Note: use k ^ 1 and k ^= 1 to switch between dp[0] (row 0) and dp[1] (row 1).
+
+    public int longestCommonSubsequence(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        if (m < n)  return longestCommonSubsequence(s2, s1);
+        int[][] dp = new int[2][n + 1];
+        for (int i = 0, k = 1; i < m; ++i, k ^= 1)
+            for (int j = 0; j < n; ++j)
+                if (s1.charAt(i) == s2.charAt(j)) dp[k][j + 1] = 1 + dp[k ^ 1][j];
+                else dp[k][j + 1] = Math.max(dp[k ^ 1][j + 1], dp[k][j]);
+        return dp[m % 2][n];
+    }
+    */
 }
