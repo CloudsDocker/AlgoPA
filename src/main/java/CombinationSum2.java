5a6dd0a41ff684969a04a1efa9edc5a86ebbe3c2
todzhang's cloudsdocker <phray.zhang@gmail.com>
2020-02-27 14:18:57 +1100

    Update CombinationSum2.java

--- src/main/java/CombinationSum2.java
+++ src/main/java/CombinationSum2.java
@@ -1,39 +1,55 @@
 class Solution {
-    /*
-    [10,1,2,7,6,1,5]
-8
-    */
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(candidates); // here is key to make array increasing
-        findCombination(candidates,0,target,new ArrayList<Integer>(),result);
-        return result;
+        public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//        List<List<Integer>> result = new ArrayList<>();
+//        Arrays.sort(candidates); // here is key to make array increasing
+//        findCombination(candidates, 0, target, new ArrayList<Integer>(), result);
+//        return result;
+
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> path = new ArrayList<Integer>();
+        findCombination(candidates, 0, target, path, res);
+        return res;
     }
-    
-    public void findCombination(int[] candidates, int idx, int target, List<Integer> current, List<List<Integer>> result){
+
+    public static void findCombination(int[] candidates, int idx, int target, List<Integer> current, List<List<Integer>> result) {
         //base case
-        if(target == 0){
+        if (target == 0) {
             // found correct combination
-            result.add(current);
+//            result.add(current);
+            result.add(new ArrayList<>(current));  //[!!!!!] Be aware there is new ArrayList(current), rather than result.add(current);, because "current" is reference , it will be changed.
             return; // should return right away after add
         }
-        
+
         // base case 2
-        if(target<0){
+        if (target < 0) {
             // last element lead to combination>target
             return;
         }
-        
-        for(int i=idx;i<candidates.length;i++){
+
+        for (int i = idx; i < candidates.length; i++) {
             // loop to try combination by DFS
-            if(i==idx || candidates[i]!=candidates[i-1]){
-                // here is key for "non dup element"
-                // as first loop is always unique, no dup
-                // for non first loop, check it with previus value
-                current.add(candidates[i]); // Not same as previous one
-                findCombination(candidates,i+1, target-candidates[i], current, result); // here will DFS try to keep on adding new element to current
-                current.remove(current.size()-1);// when above line returned, it means last element is too big// arraylist is "size()" not "length"
+
+            // appraoch 1
+//            if (i == idx || candidates[i] != candidates[i - 1]) {
+//                // here is key for "non dup element"
+//                // as first loop is always unique, no dup
+//                // for non first loop, check it with previus value
+//                current.add(candidates[i]); // Not same as previous one
+//                findCombination(candidates, i + 1, (target - candidates[i]), current, result); // here will DFS try to keep on adding new element to current
+//                current.remove(current.size() - 1);// when above line returned, it means last element is too big// arraylist is "size()" not "length"
+//            }
+
+            //approach 2
+            if(i>idx && candidates[i]==candidates[i-1]){
+                continue; // to dedup
             }
+            current.add(candidates[i]);
+            findCombination(candidates,i+1,target-candidates[i],current,result);
+            current.remove(current.size()-1);
+
+
         }
     }
+        
 }
